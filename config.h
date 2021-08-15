0ff7cd9667ce0a0bbcdb21f1b6b491685ab90b01
Louis Schräder <louis.schraeder@hs-osnabrueck.de>
2021-08-15 20:43:26 +0200

    1

---
+++ config.h
@@ -0,0 +1,15 @@
+//
+// Created by louis on 15.08.2021.
+//
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#define INTERVAL 2000
+#define SIMULATED_DATA 0
+#define BUFFER_SIZE 256
+
+#define LED_PIN 7
+
+#define CREDENTIAL_PATH "~/.iot-hub"
+
+#endif  // CONFIG_H_
