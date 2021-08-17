17ce00452ee9765e91a65b52a7dfda4f1ae76a47
Louis Schräder <louis.schraeder@hs-osnabrueck.de>
2021-08-17 22:26:13 +0200

    47

--- soil_moisture_sensor.c
+++ soil_moisture_sensor.c
@@ -12,10 +12,18 @@
 
 int readEarth()
 {
+    wiringPiSetupGpio();
+    pinMode (gpioEarth, INPUT);
     int value = 0;
 
     //value = digitalRead(gpioEarth);
-
+    while(1) {
+        if (digitalRead(PIN_BUTTON) == LOW) {
+            printf("Button pressed\n");
+            delay(200);
+        }
+        delay(1);
+    }
     printf( "Earth: %d\n", value);
 
     return 0;
