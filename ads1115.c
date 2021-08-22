a628acd6f448e7d88253c7ffc7aa202f0e477422
Louis Schräder <louis.schraeder@hs-osnabrueck.de>
2021-08-22 17:58:23 +0200

    115

--- ads1115.c
+++ ads1115.c
@@ -120,14 +120,14 @@ int readAnalog(int * earth, int * rain) {
 
     int in_min, in_max, out_min, out_max;
     in_min = 32767;
-    in_max = 19365;
+    in_max = 12365;
     out_min = 0;
     out_max = 100;
 
     int valEarthOut = (valEarth - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 
     in_min = 32767;
-    in_max = 12811;
+    in_max = 7811;
     out_min = 0;
     out_max = 100;
 
